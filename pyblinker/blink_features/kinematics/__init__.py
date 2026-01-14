69f77d84c0fc70e5df5bf69d06f8ac7ad04ea686
RODNEY PETRUS BALANDONG <balandongiv@gmail.com>
2026-01-14 10:36:12 +0800

    Use kinematics extractor in EOG-only test

--- pyblinker/blink_features/kinematics/__init__.py
+++ pyblinker/blink_features/kinematics/__init__.py
@@ -1,10 +1,10 @@
-"""Blink kinematic feature package."""
+"""Public exports for blink kinematic features."""
 
 from .core_metrics import compute_blink_kinematic_metrics
 from .kinematic_features import KinematicBlinkFeatureExtractor, compute_kinematic_features
 
-__all__ = [
+__all__ = (
+    "KinematicBlinkFeatureExtractor",
     "compute_blink_kinematic_metrics",
     "compute_kinematic_features",
-    "KinematicBlinkFeatureExtractor",
-]
+)
